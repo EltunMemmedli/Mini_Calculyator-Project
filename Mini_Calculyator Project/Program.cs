7425390ef6cad2df8d9152b91667e239d22068f7
Eltun Məmmədli <meltun4255540@gmail.com>
2024-09-01 15:37:24 +0400

    Add project files.

---
+++ Mini_Calculyator Project/Program.cs
@@ -0,0 +1,77 @@
+﻿// Birinci eded
+Main:
+
+Console.Write("Birinci ededi daxile edin: ");
+decimal birinci_eded = decimal.Parse(Console.ReadLine());
+
+// Ikinci eded
+Console.Write("Ikinci ededi daxil edin: ");
+decimal ikinci_eded = decimal.Parse(Console.ReadLine());
+
+// Emeli daxil edin
+Console.Write("Hansi emel daxil edeceksiniz: ");
+string emel = Console.ReadLine();
+
+//Toplayir
+if (emel == "+")
+{
+    Console.WriteLine(Plus(birinci_eded, ikinci_eded));
+    goto Main;
+
+}
+else if (emel == "-")
+{
+    Console.WriteLine(Minus(birinci_eded, ikinci_eded));
+    goto Main;
+
+}
+else if (emel == "*")//Vurma
+{
+    Console.WriteLine(Hasil(birinci_eded, ikinci_eded));
+    goto Main;
+
+}
+else if (emel == "/")//Bolme
+{
+    Console.WriteLine(Dividing(birinci_eded, ikinci_eded));
+    goto Main;
+}
+else
+{
+    goto Main;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+// Toplama emeli
+decimal Plus(decimal num_1, decimal num_2)
+{
+    return num_1 + num_2;
+}
+
+// Cixma emeli
+decimal Minus(decimal num_1, decimal num_2)
+{
+    return num_1 - num_2;
+}
+
+// Vurma emeli
+decimal Hasil(decimal num_1, decimal num_2)
+{
+    return num_1 * num_2;
+}
+
+// Bolme emeli
+decimal Dividing(decimal num_1, decimal num_2)
+{
+    return num_1 / num_2;
+}
